7e2ddc356d4a722aa98b332228a4e60b598f3187
Cristina Preciado <preciadobeltran232@lcsd2kids.org>
2021-05-06 11:02:15 -0600

    added new grid button

--- script.js
+++ script.js
@@ -2,21 +2,16 @@ const gridContainer = document.getElementById("grid-container");
 let userInput = 16;
 for (let index = 0; index < userInput; index++) {
     let rowCon = document.createElement("div")
-   
-    
-
-
-
-for(let i = 0;i<userInput;i++){
-    const singlecell = document.createElement("div");
-    singlecell.textContent = ".";
-    singlecell.style.color = "bg-red";
-    singlecell.classList.add("bg-red");
-singlecell.addEventListener('mouseover' , () => {
-    singlecell.classList.add("bg-blue")
-    singlecell.style.color = "bg-blue";
-    });
-rowCon.appendChild(singlecell);
-}
+    for(let i = 0;i<userInput;i++){
+        const singlecell = document.createElement("div");
+        singlecell.textContent = ".";
+        singlecell.style.color = "bg-red";
+        singlecell.classList.add("bg-red");
+        singlecell.addEventListener('mouseover' , () => {
+        singlecell.classList.add("bg-blue")
+        singlecell.style.color = "bg-blue";
+        });
+    rowCon.appendChild(singlecell);
+    }
 gridContainer.appendChild(rowCon);
 }
