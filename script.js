588ccec27722c567d0955e80b242977618ae6066
Cristina Preciado <preciadobeltran232@lcsd2kids.org>
2021-05-10 11:32:29 -0600

    done with button

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const gridContainer = document.getElementById("grid-container");
 let userInput = 16;
-for (let index = 0; index < userInput; index++) {
-    let rowCon = document.createElement("div")
-    for(let i = 0;i<userInput;i++){
-        const singlecell = document.createElement("div");
-        singlecell.textContent = ".";
-        singlecell.style.color = "bg-red";
-        singlecell.classList.add("bg-red");
-        singlecell.addEventListener('mouseover' , () => {
-        singlecell.classList.add("bg-blue")
-        singlecell.style.color = "bg-blue";
-        });
-    rowCon.appendChild(singlecell);
+
+function buildGrid(){
+    for (let index = 0; index < userInput; index++) {
+        let rowCon = document.createElement("div")
+        for(let i = 0;i<userInput;i++){
+            const singlecell = document.createElement("div");
+            singlecell.textContent = ".";
+            singlecell.style.color = "bg-red";
+            singlecell.classList.add("bg-red");
+            singlecell.addEventListener('mouseover' , () => {
+            singlecell.classList.add("bg-blue")
+            singlecell.style.color = "bg-blue";
+            });
+        rowCon.appendChild(singlecell);
+        }
+    gridContainer.appendChild(rowCon);
     }
-gridContainer.appendChild(rowCon);
+    
 }
+const newGrid = document.getElementById("new-grid-button");
+newGrid.addEventListener('click' , () => {
+
+    userInput = prompt("How many boxes per side?");
+    gridContainer.innerHTML = "";
+    buildGrid();
+});
+
+buildGrid();
